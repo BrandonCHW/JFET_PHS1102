4b9bdb3a657c21a65026c68460e819d0b4fbc779
Brandon Chan <brad_1800@hotmail.com>
2017-10-26 01:32:49 -0400

    Code JFET fini

--- Devoir PHS1102/JFET.cpp
+++ Devoir PHS1102/JFET.cpp
@@ -57,56 +57,119 @@ void Jfet::setpot(const double& pot, const int& x, const int& y) const
 	potentials[x][y].setValue(pot);
 }
 
-/*
-void Jfet::fillpots(const double** &list) 
+// Marque les potentiels qui ne peuvent pas etre modifies (constants)
+void Jfet::setPotsModif()
 {
-	for (int i = 0; i < getsize_x(); ++i) {
-		for (int j = 0; j < getsize_y(); ++j) {
-			double val = list[i][j];
-			setpot(val, i, j);
-		}
-	}
-			
-}*/
-
-void Jfet::initializePots()
-{
-	for (int toprow = 1; toprow <= 3; toprow++) // grille du haut
-		for (int topcol = 7; topcol <= 9; topcol++) {
+	for (int toprow = 0; toprow <= 3; toprow++) // grille du haut
+		for (int topcol = 5; topcol <= 11; topcol++) {
 			potentials[toprow][topcol].setModif(false);
 		}
 
-	for (int bottomrow = 11; bottomrow <= 13; bottomrow++) // grille du base
-		for (int bottomcol = 7; bottomcol <= 9; bottomcol++) {
+	for (int bottomrow = 9; bottomrow <= 12; bottomrow++) // grille du base
+		for (int bottomcol = 5; bottomcol <= 11; bottomcol++) {
 			potentials[bottomrow][bottomcol].setModif(false); 
 	}
+
+	for (int sourcerow = 0; sourcerow <= 4; sourcerow++) // potentiels a la source
+		potentials[sourcerow][0].setModif(false);
+
+	for (int drainrow = 8; drainrow <= 12; drainrow++) // potentiels au drain
+		potentials[drainrow][16].setModif(false);
 }
 
-void Jfet::finiteDifferenceMethod(const int& interations, const double& convergence, const double& maxIterations)
+void Jfet::finiteDifferenceMethod(const double& Seuil, const double& MaxItera)
 {
-	for (int i = 1; i < getsize_x() - 2; ++i) {
-		for (int j = 1; j < getsize_y() - 2; ++j) {
-			if (getpot(i, j).isModif()) //le potentiel doit etre modifiable
+	double v1, v2, v3, v4 = 0.0; //potentiels autour du point recherche. 
+							   //v0 = potentiel recherche. v1 = pot. gauche, v2 = pot. haut, v3 = bas, v4 = droite
+	double vertical, horizontal = 0;
+	bool isVerticalEdge, isHorizontalEdge;
 
-				double v0, v1, v2, v3, v4 = 0; //potentiels autour du point recherche. 
-										   //v0 = potentiel recherche. v1 = pot. gauche, v2 = pot. haut, v3 = bas, v4 = droite
-				
+	unsigned Nitera = 0; // nombre d'iterations presentement
+	bool Convergence = false;
+	double Vnouveau = 0.0;
+
+	
+while (!Convergence && Nitera <= MaxItera) {
+		Nitera++;
+		Convergence = true;
+	
+		for (int row = 0; row < getsize_x(); ++row) { // pour chaque point
+			for (int col = 0; col < getsize_y(); ++col) {
+				if (getpot(row, col).isModif()) { //verifier que le potentiel peut etre modifiable
+
+					vertical = 0;
+					horizontal = 0;
+					isVerticalEdge = false;
+					isHorizontalEdge = false;
+
+					if (col - 1 < 0) { // potentiel a la frontiere conducteur/dielectrique gauche
+						v1 = 0; //aucun potentiel
+						isVerticalEdge = true;
+					}
+					else {
+						v1 = getpot(row, col - 1).getValue();
+					}
+
+					if (col + 1 > getsize_y() - 1) {// potentiel a la frontiere conducteur/dielectrique droite
+						v4 = 0;
+						isVerticalEdge = true;
+					}
+					else
+						v4 = getpot(row, col + 1).getValue();
+
+					if (row - 1 < 0) {// potentiel a la frontiere conducteur/dielectrique haut
+						v2 = 0;
+						isHorizontalEdge = true;
+					}
+					else
+						v2 = getpot(row - 1, col).getValue();
+										
+					if (row + 1 > getsize_x() - 1) {
+						v3 = 0;
+						isHorizontalEdge = true;
+					}
+					else
+						v3 = getpot(row + 1, col).getValue();
+
+					vertical = v2 + v3;
+					horizontal = v1 + v4;
+					
+					//moyenne
+					if (isVerticalEdge && !isHorizontalEdge) //est un interface conducteur/dielectrique vertical
+						Vnouveau = (2 * vertical + horizontal) / 2;
+
+					else if (!isVerticalEdge && isHorizontalEdge) //est un interface conducteur/dielectrique horizontal
+						Vnouveau = (2 * horizontal + vertical) / 2;
+
+					else if (isVerticalEdge && isHorizontalEdge) // est un interface dielectrique/coin conducteur
+						Vnouveau = (vertical + horizontal) / 2;
+
+					else if (!isVerticalEdge && !isHorizontalEdge)
+						Vnouveau = (vertical + horizontal) / 4;
+
+					//voir si difference est plus grande que le seuil
+					if (abs(Vnouveau - potentials[row][col].getValue()) > Seuil)
+						Convergence = false;
+
+					potentials[row][col].setValue(abs(Vnouveau - potentials[row][col].getValue()));
+				}				
+			}
 		}
 	}
 }
 
 void Jfet::printpots() 
 {
-	cout << setprecision(2) << fixed;
+	cout << setprecision(8) << fixed;
 	cout << "Tableau des potentiels du JFET \n\n";
 
-	int blank = 8; //utilise pour espacement
+	int blank = 13; //utilise pour espacement
 
 	for (int i = 0; i < getsize_x() - 0; ++i) { // affiche seulement l'interieur (pas la frontiere)
 		for (int j = 0; j < getsize_y() - 0; ++j) { //affiche seulement l'interieur 
 
 			if (getpot(i, j).getValue() > 100.0 ) {
-				cout << setw(blank) << getpot(i, j).getValue();
+				cout << setw(blank) << getpot(i, j).getValue(); 
 			}
 			else {
 				cout << setw(blank) << getpot(i, j).getValue();
---
+++ Devoir PHS1102/JFET.h
@@ -0,0 +1,39 @@
+#ifndef JFET_H
+#define JFET_H
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include "Potential.h"
+
+using namespace std;
+
+class Jfet
+{
+public:
+	Jfet();
+	Jfet(const int& x, const int& y);
+	~Jfet();
+	
+	//getters
+	int getsize_x() const;
+	int getsize_y() const;
+	Potential getpot(const int& x, const int& y) const;
+
+	//setters
+	void setsize_x(const int& size);
+	void setsize_y(const int& size);
+	void setpot(const double& pot, const int& x, const int& y) const;
+
+	//jfet methods
+	void setPotsModif();
+	void finiteDifferenceMethod(const double& Seuil, const double& MaxItera);
+	void printpots();
+	
+private:
+	int size_x;
+	int size_y;
+	Potential** potentials;
+};
+
+#endif
